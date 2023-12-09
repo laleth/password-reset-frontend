89f71eb541e4be0b6201e16d2a475d7f8db15e9f
laleth <trlaleth@gmail.com>
2023-12-09 17:34:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 //import ResetPasswordPage from './components/ResetPasswordPage';
 
